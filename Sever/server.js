53741bbef90bfd6d13787c4b5bb453e2472186e0
Yuvraj-508 <yuvi97437@gmail.com>
2026-07-26 21:35:07 +0530

    api

--- Sever/server.js
+++ Sever/server.js
@@ -18,7 +18,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://urban-kicks-two.vercel.app",
+    ],
     credentials: true,
   })
 );
